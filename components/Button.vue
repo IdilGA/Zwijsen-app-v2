da0d6eff5ff983a5ecb76bef71dd71a7ff9c3267
Daanisaanwezig <daanisaanwezig@outlook.com>
2025-06-02 15:24:25 +0200

    Add header and button component (unfinished)

---
+++ components/Button.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+    type: string;
+    disabled?: boolean;
+}>()
+</script>
+<template>
+    <button :class="type" :disabled="disabled">
+        <slot></slot>
+    </button>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+    backButton?: boolean;
+}>()
+</script>
+<template>
+    <header class="header">
+        <h2 class="header__title"><slot /></h2>
+    </header>
+</template>
+<style lang="scss">
+.header__title {
+    text-align: center;
+}
+</style>
