421341e32db2213a7d6562339f0a4c97e21b8a18
Idil <i364923@fhict.nl>
2025-06-02 15:54:19 +0200

    add links to homebuttons

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <script setup>
-import Header from '~/components/Header.vue';
-import Button from '~/components/Button.vue';
+import Header from "~/components/Header.vue";
+import Button from "~/components/Button.vue";
 </script>
 <template>
-    <Header>Home</Header>
-    <div class="home-buttons">
-        <Button>Scan toetsen</Button>
-        <Button>Bekijk eerdere scans</Button>
-    </div>
+  <Header>Home</Header>
+  <div class="home-buttons">
+    <Button @click="$router.push('/scan')"> Scan toetsen </Button>
+    <Button @click="$router.push('/scans')">Bekijk eerdere scans</Button>
+  </div>
 </template>
 <style lang="scss">
 .home-buttons {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 </style>
