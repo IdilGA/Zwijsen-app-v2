2ad81712304ca642fb7986446a11de810b2f0355
Daanisaanwezig <daanisaanwezig@outlook.com>
2025-06-04 12:25:15 +0200

    Add button styling

--- components/Button.vue
+++ components/Button.vue
@@ -5,7 +5,28 @@ const props = defineProps<{
 }>()
 </script>
 <template>
-    <button :class="type" :disabled="disabled">
+    <button :class="type" class="btn" :disabled="disabled">
         <slot></slot>
     </button>
 </template>
+<style lang="scss">
+.btn {
+    width: 100%;
+    padding: 12px;
+    color: var(--color-text);
+    font-weight: bold;
+    font-size: 16px;
+    border: none;
+    outline: none;
+    border-radius: 12px;
+    &.primary {
+        background-color: var(--color-primary);
+    }
+    &.secondary {
+        background-color: var(--color-secondary);
+    }
+    &.light {
+        background-color: var(--color-light);
+    }
+}
+</style>
